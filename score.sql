c0839756a0e0f009d3dd3a4b138db69d95ee4ce2
sagarjha1846 <61563265+sagarjha1846@users.noreply.github.com>
2020-08-12 13:30:44 +0530

    Add files via upload

---
+++ score.sql
@@ -0,0 +1,11 @@
+(null, 10, 'qqw', 90283622, 12, 'Female', 'BGG', '2012-06-02', 'Mumbai', '2020-12-16', 'India', 'NO', 'Q1', 45, 2, -1, 'Attempted', 'A', 'A', 'Correct', 2,0),
+(null, 10, 'qqw', 90283622, 12, 'Female', 'BGG', '2012-06-02', 'Mumbai', '2020-12-16', 'India', 'NO', 'Q2', 35, 2, -1, 'Unattempted', '', 'B', 'Unattempted', 0,0),
+(null, 10, 'qqw', 90283622, 12, 'Female', 'BGG', '2012-06-02', 'Mumbai', '2020-12-16', 'India', 'NO', 'Q3', 56, 2, -1, 'Attempted', 'C', 'C', 'Correct', 2,0),
+(null, 10, 'qqw', 90283622, 12, 'Female', 'BGG', '2012-06-02', 'Mumbai', '2020-12-16', 'India', 'NO', 'Q4', 91, 2, -1, 'Attempted', 'D', 'D', 'Correct', 2,0),
+(null, 10, 'qqw', 90283622, 12, 'Female', 'BGG', '2012-06-02', 'Mumbai', '2020-12-16', 'India', 'NO', 'Q5', 36, 2, -1, 'Attempted', 'D', 'E', 'Incorrect', -1,0);
+						
+	10	qqw	90283622	12	F	BGG	2-Jun-12	Mumbai	16-Dec-20	India	No	Q1	45	2	-1	Attempted	A	A	Correct	2							
+	10	qqw	90283622	12	F	BGG	2-Jun-12	Mumbai	16-Dec-20	India	No	Q2	35	2	-1	Unattempted		B	Unattempted	0							
+	10	qqw	90283622	12	F	BGG	2-Jun-12	Mumbai	16-Dec-20	India	No	Q3	56	2	-1	Attempted	C	C	Correct	2							
+	10	qqw	90283622	12	F	BGG	2-Jun-12	Mumbai	16-Dec-20	India	No	Q4	91	2	-1	Attempted	D	D	Correct	2							
+	10	qqw	90283622	12	F	BGG	2-Jun-12	Mumbai	16-Dec-20	India	No	Q5	36	2	-1	Attempted	D	E	Incorrect	-1							
